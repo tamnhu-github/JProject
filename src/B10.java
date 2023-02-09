747083a3e85e7bdb37f9607621507053c85a70a4
Trương Nguyễn Tâm Như <97598854+tamnhu-github@users.noreply.github.com>
2023-02-09 22:45:52 +0700

    final commit n

--- src/B10.java
+++ src/B10.java
@@ -1,16 +1,11 @@
-
+import java.lang.Math;
 public class B10 {
 	static boolean checkFibonacci(int n) {
-		if (n ==0 || n == 1) return true;
-		int f1 = 1;
-		int f2 = 0;
-		for (int i = 3; i<93; i++) {
-			int f = f1 + f2;
-			if (f == n) return true;
-			f2 = f1;
-			f1 = f;
-		}
-		return false;
+		int a = 5 * n * n + 4;
+		int b = 5 * n * n - 4;
+		double sqrt_a = Math.sqrt(a);
+		double sqrt_b = Math.sqrt(b);
+		return (sqrt_a * sqrt_a == a)  || (sqrt_b * sqrt_b == b);
 	}
 	static void lietKe(int n) {
 		System.out.print("So Fibonacci nho hon n va la so nguyen to: ");
