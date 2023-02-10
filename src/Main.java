80395d093eea555ca7de0ccc7b77cf5d6fdcb55d
Trương Nguyễn Tâm Như <97598854+tamnhu-github@users.noreply.github.com>
2023-02-10 07:18:12 +0700

    changed n 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		boolean flag = true;
 		int a,b,c,n;
